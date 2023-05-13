35bda1006ef9fef49d854db1b70a2c49d551ff19
teguchi0920 <childrenkey@gmail.com>
2023-05-13 21:41:48 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'books#index'
 end
