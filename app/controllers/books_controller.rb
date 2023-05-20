8c153c5d6565c1065c9e4c0c22c06839f54af2f1
teguchi0920 <childrenkey@gmail.com>
2023-05-20 20:58:03 +0900

    createアクションを定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,13 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
