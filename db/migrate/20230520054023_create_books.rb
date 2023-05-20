3e1e277e019e18fae94860cc534493028c8aeb45
teguchi0920 <childrenkey@gmail.com>
2023-05-20 14:55:52 +0900

    マイグレーションを実行

--- db/migrate/20230520054023_create_books.rb
+++ db/migrate/20230520054023_create_books.rb
@@ -1,7 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
-
+      t.string     :title,             null: false
+      t.text       :impress,           null: false
+      t.integer    :book_category_id,  null: false
+      t.references :user,              null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_14_115836) do
+ActiveRecord::Schema.define(version: 2023_05_20_054023) do
+
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "impress", null: false
+    t.integer "book_category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2023_05_14_115836) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
